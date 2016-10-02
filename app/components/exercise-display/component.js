05055e28b710da3aca7800ca83908b0ba35b0ea9
natGalynker <natalgal@gmail.com>
2016-10-02 12:06:57 -0400

    Create Put for Exercise
    add a done send action and create an Exercise done action that will
    toggle to done when clicked
    create a button for that action
    and inoke that component in the template holding the exercises

--- app/components/exercise-display/component.js
+++ app/components/exercise-display/component.js
@@ -1,12 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // classNameBindings: ['exerciseCompleted'],
-  // exerciseCompleted: Ember.computed.alias('exercise.done'),
     actions: {
-      // done () {
-      //   this.sendAction('done', this.get('exercise'));
-      // },
+      done () {
+        this.sendAction('done', this.get('exercise'));
+      },
       delete(){
         this.sendAction('delete', this.get('exercise'));
         console.log('word');
