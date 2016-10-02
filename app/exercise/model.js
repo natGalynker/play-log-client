0bb9bfdae1080fdc6f74bb26b3e1ba918f21400f
natGalynker <natalgal@gmail.com>
2016-10-02 15:55:18 -0400

    Parse data on create
    edit the invoking of the template
    create relationship between the send action and the
    create action. Send the actions up to get the data back down

--- app/exercise/model.js
+++ app/exercise/model.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 category: DS.attr('string'),
 name: DS.attr('string'),
+user: DS.belongsTo('user'),
 description: DS.attr('string'),
 duration: DS.attr('string'),
 done: DS.attr('boolean')
--- app/exercises/new/route.js
+++ app/exercises/new/route.js
@@ -1,16 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('exercise');
+  },
 
   actions: {
-  createExercise(data){
-    let exercise = this.get('store').createRecord('exercise', data);
-    console.log(data);
-    console.log('slut');
-    exercise.save()
-    .then(()=> this.transitionTo('exercises'));
+
+    saveExercise(createExercise) {
+      createExercise.save()
+      .then(() => this.transitionTo('exercises'));
+    },
   }
-}
+//   actions: {
+//   createExercise(data){
+//     let exercise = this.get('store').createRecord('exercise', data);
+//     console.log(data);
+//     console.log('slut');
+//     exercise.save()
+//     .then(()=> this.transitionTo('exercises'));
+//   }
+// }
 });
 
 //     model () {
