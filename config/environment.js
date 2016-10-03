fff17a0a77d4452b34748548ecee225e7f53fa4e
natGalynker <natalgal@gmail.com>
2016-10-02 21:44:50 -0400

    Update environment
    alter the naming conventions for deploy

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'ga-wdi-boston.ember-auth',
+    modulePrefix: 'client-play-log',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
